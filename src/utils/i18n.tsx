54a5347700a60078fe372dbd5472c9c8063aa0f2
Dev <dev@gmail..com>
2026-03-12 15:26:39 +0700

    Update: Successfully

--- src/utils/i18n.tsx
+++
@@ -1,8 +0,0 @@
-import type { Lang } from "../types/Project";
-
-export const getTranslatedText = (
-  text: string | Record<Lang, string>,
-  lang: Lang
-): string => {
-  return typeof text === "string" ? text : text[lang];
-};
