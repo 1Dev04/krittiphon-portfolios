dd53a8b59e04c0f1c96f7d7949584c4518ca0459
Dev <dev@gmail..com>
2026-03-18 14:02:53 +0700

    Update: Click to Mobile Web APP

--- src/pages/FeatureProject.tsx
+++ src/pages/FeatureProject.tsx
@@ -805,7 +805,7 @@ export default function FeatureProjectPage() {
                 }}
               >
                 <img
-                  src="https://res.cloudinary.com/dag73dhpl/image/upload/v1773814291/image-removebg-preview_fzx9e6.png"
+                  src="https://res.cloudinary.com/dag73dhpl/image/upload/v1773817546/image-removebg-preview_n5i6gx.png"
                   alt="ABCat mascot — click to visit app"
                   style={{ width: "100%", display: "block" }}
                 />
